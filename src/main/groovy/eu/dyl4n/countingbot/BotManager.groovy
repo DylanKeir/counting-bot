b4646207a4d46578f47ae0f5e7700c127c4ac742
Dylan Keir <me@dyl4n.me>
2020-03-04 08:59:08 +0000

    thiimo got small knees

--- src/main/groovy/eu/dyl4n/countingbot/BotManager.groovy
+++ src/main/groovy/eu/dyl4n/countingbot/BotManager.groovy
@@ -1,35 +1,34 @@
 package eu.dyl4n.countingbot
 
-
-import net.dv8tion.jda.core.JDABuilder
-import net.dv8tion.jda.core.entities.Message
-import net.dv8tion.jda.core.events.Event
-import net.dv8tion.jda.core.events.message.MessageReceivedEvent
-import net.dv8tion.jda.core.events.message.MessageUpdateEvent
-import net.dv8tion.jda.core.hooks.EventListener
+import net.dv8tion.jda.api.JDABuilder
+import net.dv8tion.jda.api.entities.Message
+import net.dv8tion.jda.api.events.GenericEvent
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent
+import net.dv8tion.jda.api.events.message.MessageUpdateEvent
+import net.dv8tion.jda.api.hooks.EventListener
 
 class BotManager {
 
     static def jda
 
     BotManager() {
-        jda = new JDABuilder("aa")
-                .addEventListener(new EventListener() {
+        jda = new JDABuilder("token")
+                .addEventListeners(new EventListener() {
                     @Override
-                    void onEvent(Event event) {
+                    void onEvent(GenericEvent event) {
                         if (!(event instanceof MessageReceivedEvent)) {
                             return
                         }
 
                         def msg = event.message
 
-                        if (msg.channel.idLong != 571152815003795477) {
+                        if (msg.channel.idLong != 684631265596407838) {
                             return
                         }
 
-                        def i = getNumberFromMessage msg, true
-                        def lastMsg = msg.channel.history.retrievePast(2).complete().get 1
-                        def lastMsgI = getNumberFromMessage lastMsg, false
+                        def i = getNumberFromMessage(msg, true)
+                        def lastMsg = msg.channel.history.retrievePast(2).complete().get(1)
+                        def lastMsgI = getNumberFromMessage(lastMsg, false)
 
                         try {
                             if (i == (lastMsgI as int) + 1) {
@@ -41,15 +40,15 @@ class BotManager {
 
                         msg.delete().complete()
                     }
-                }).addEventListener(new EventListener() {
+                }, new EventListener() {
                     @Override
-                    void onEvent(Event event) {
+                    void onEvent(GenericEvent event) {
                         if (!(event instanceof MessageUpdateEvent)) {
                             return
                         }
 
                         def msg = event.getMessage()
-                        if (msg.channel.idLong != 571152815003795477) {
+                        if (msg.channel.idLong != 684631265596407838) {
                             return
                         }
 
@@ -58,11 +57,11 @@ class BotManager {
                 }).build()
     }
 
-    def getNumberFromMessage(Message msg) throws NumberFormatException {
+    static def getNumberFromMessage(Message msg) throws NumberFormatException {
         return Integer.parseInt(msg.contentDisplay)
     }
 
-    def getNumberFromMessage(Message msg, boolean delete) {
+    static def getNumberFromMessage(Message msg, boolean delete) {
         try {
             return getNumberFromMessage(msg)
         } catch (NumberFormatException e) {
--- src/main/groovy/eu/dyl4n/countingbot/Main.groovy
+++ src/main/groovy/eu/dyl4n/countingbot/Main.groovy
@@ -1,16 +1,16 @@
 package eu.dyl4n.countingbot
 
-import net.dv8tion.jda.core.JDA
+import net.dv8tion.jda.api.JDA
 
 class Main {
 
     static def botManager
 
     static void main(String[] args) {
-        this.botManager = new BotManager()
+        botManager = new BotManager()
 
         Thread.currentThread().addShutdownHook {
-            ((this.botManager as BotManager).jda as JDA).shutdownNow()
+            ((botManager as BotManager).jda as JDA).shutdownNow()
         }
     }
 
